99569b33aefa5ccc9e119f10f18a53ba62a7be7d
Artem Smirnov <urpylka@gmail.com>
2020-08-09 18:30:19 +0300

    Update keys scripts

--- assets/keys_gen.sh
+++ assets/keys_gen.sh
@@ -3,7 +3,11 @@
 # Copyright 2018-2020 Artem B. Smirnov
 # Licensed under the Apache License, Version 2.0
 
-# Use: gen_keys.sh <FULL_NAME> <EMAIL_ADDRESS> <GPG_PASSPHRASE>
+# Use: keys_gen.sh <FULL_NAME> <EMAIL_ADDRESS> <GPG_PASSPHRASE>
+
+[[ -z ${1} ]] && exit 1;
+[[ -z ${2} ]] && exit 1;
+[[ -z ${3} ]] && exit 1;
 
 # https://stackoverflow.com/questions/4437573/bash-assign-default-value
 : ${FULL_NAME:=${1}}
@@ -11,11 +15,6 @@
 : ${GPG_PASSPHRASE:=${3}}
 
 gen_batch() {
-
-  [[ -z $1 ]] && exit 1;
-  [[ -z $2 ]] && exit 1;
-  [[ -z $3 ]] && exit 1;
-
   cat << EOF > /opt/gpg_batch
 %echo Generating a GPG key, might take a while
 Key-Type: RSA
@@ -27,36 +26,40 @@ Name-Comment: Aptly Repo Signing
 Name-Email: ${2}
 Expire-Date: 0
 Passphrase: ${3}
-%pubring /opt/aptly/aptly.pub
-%secring /opt/aptly/aptly.sec
+%pubring /opt/aptly/gpg/pubring.gpg
+%secring /opt/aptly/gpg/secring.gpg
 %commit
 %echo done
 EOF
 }
 
 # If the repository GPG keypair doesn't exist, create it.
-if [[ ! -f /opt/aptly/aptly.sec ]] || [[ ! -f /opt/aptly/aptly.pub ]]; then
-  echo "Generating new GPG keys"
+if [[ ! -f /opt/aptly/gpg/secring.gpg ]] || [[ ! -f /opt/aptly/gpg/pubring.gpg ]]; then
+  echo "Generating the new GPG keypair"
   cp -a /dev/urandom /dev/random
 
-  # Generate GPG config for generating new keypair
+  mkdir -p /opt/aptly/gpg
+
+  # Generate the GPG config for generating the new keypair
   gen_batch ${FULL_NAME} ${EMAIL_ADDRESS} ${GPG_PASSPHRASE}
 
   # If your system doesn't have a lot of entropy this may, take a long time
-  # Google how-to create "artificial" entropy if this gets stuck
-  gpg --batch --gen-key /opt/gpg_batch
+  # Google how-to create "artificial" entropy, if this gets stuck
+  gpg --gen-key --batch /opt/gpg_batch
 
-  # Remove batch after generating keypair
+  # Remove batch after generating the keypair
   rm /opt/gpg_batch
 else
-  echo "No need to generate new GPG keys"
+  echo "No need to generate the new GPG keypair"
 fi
 
 # If the repository public key doesn't exist, export it.
 if [[ ! -d /opt/aptly/public ]] || [[ ! -f /opt/aptly/public/aptly_repo_signing.key ]]; then
-  echo "Export the GPG public key"
+  echo "Export the GPG public keys"
   mkdir -p /opt/aptly/public
+  # Export only all public keys,
+  # for export private keys use --export-secret-keys
   gpg --export --armor > /opt/aptly/public/aptly_repo_signing.key
 else
-  echo "No need to export GPG keys"
+  echo "No need to export the GPG keys"
 fi
---
+++ assets/keys_imp.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# Copyright 2018-2020 Artem B. Smirnov
+# Copyright 2018 Jon Azpiazu
+# Copyright 2016 Bryan J. Hong
+# Licensed under the Apache License, Version 2.0
+
+# Use: keys_imp.sh <path-to-keyring.gpg>
+
+# Import keyrings if they exist
+if [[ -f ${1} ]]; then
+  # Export all public keys from a keyring or a key
+  # ${1} to /opt/aptly/gpg/trustedkeys.gpg
+
+  I=${1}
+  O="/opt/aptly/gpg/trustedkeys.gpg"
+
+  gpg --no-options --no-default-keyring --keyring ${I} --export | \
+  gpg --no-options --no-default-keyring --keyring ${O} --import
+
+  # gpg --no-options --no-default-keyring --keyring ${O} --list-keys
+fi
