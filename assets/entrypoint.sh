29228335166bf734e6fa382767df25c45a370fd5
Artem Smirnov <urpylka@gmail.com>
2021-09-26 20:56:24 +0400

    Fixes entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -11,10 +11,10 @@ auto_envsubst() {
 
   local template defined_envs relative_path output_path subdir
   defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
-  [ -d "$template_dir" ] || return 0
+  [ -d "$template_dir" ] || return 1
   if [ ! -w "$output_dir" ]; then
-    echo >&3 "$ME: ERROR: $template_dir exists, but $output_dir is not writable"
-    return 0
+    echo "$ME: ERROR: $template_dir exists, but $output_dir is not writable"
+    return 1
   fi
   find "$template_dir" -follow -type f -name "*$suffix" -print | while read -r template; do
     relative_path="${template#$template_dir/}"
@@ -22,7 +22,7 @@ auto_envsubst() {
     subdir=$(dirname "$relative_path")
     # create a subdirectory where the template file exists
     mkdir -p "$output_dir/$subdir"
-    echo >&3 "$ME: Running envsubst on $template to $output_path"
+    echo "$ME: Running envsubst on $template to $output_path"
     envsubst "$defined_envs" < "$template" > "$output_path"
   done
 }
